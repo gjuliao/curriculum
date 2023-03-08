b1cf37825c02740509c01e68ec7a395abf0792ea
Giovanni Juliao <gjuliao32@gmail.com>
2023-03-08 10:32:07 -0500

    Added association of book with rental

--- student.rb
+++ student.rb
@@ -3,7 +3,7 @@ require_relative 'person'
 class Student < Person
   attr_reader :classroom
   def initialize(classroom:, name: 'Unknown', age: 0, parent_permission: true)
-    super(name: name, age: age, parent_permission: parent_permission)
+    super(name, age)
     @classroom = classroom
   end
 
