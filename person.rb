66c405e90afdb32a4f41cf1fb949b1d7090a2785
Giovanni Juliao <gjuliao32@gmail.com>
2023-03-08 10:46:09 -0500

    Added person, rental association

--- person.rb
+++ person.rb
@@ -1,7 +1,7 @@
 require_relative 'nameable'
 class Person < Nameable
   attr_reader :id
-  attr_accessor :name, :age, :parent_permission
+  attr_accessor :name, :age, :rental
 
   def initialize(name: 'Unknown', age: 0, parent_permission: true)
     super()
@@ -9,6 +9,7 @@ class Person < Nameable
     @name = name
     @age = age
     @parent_permission = parent_permission
+    @rentals = []
   end
 
   def can_use_service?
--- rental.rb
+++ rental.rb
@@ -1,7 +1,11 @@
 class Rental
-    attr_accessor :date
+    attr_accessor :date, :person, :book
 
-    def initialize(date)
+    def initialize(date, book, person)
         @date = date
+        @person = person
+        @book = book
+        book.rentals.push(self)
+        person.rentals.push(self)
     end
 end
