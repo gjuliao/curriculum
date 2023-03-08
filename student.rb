33c7025a9d265b9bfde8c9b192d85cd791a96930
Giovanni Juliao <gjuliao32@gmail.com>
2023-03-08 11:24:29 -0500

    Added classroom student association

--- student.rb
+++ student.rb
@@ -5,10 +5,14 @@ class Student < Person
 
   def initialize(classroom:, name: 'Unknown', age: 0)
     super(name, age)
-    @classroom = classroom
   end
 
   def play_hooky
     '¯(ツ)/¯'
+  end
+
+  def classroom=(classroom)
+    @classroom = classroom
+    classroom.students.push(self) unless classroom.students.include?(self)
   end
 end
