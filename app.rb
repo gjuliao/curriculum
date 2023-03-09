4a597dc9740a4633aef9969b90054011bfcdfd62
Giovanni Juliao <gjuliao32@gmail.com>
2023-03-09 07:20:42 -0500

    Created book, teacher, student, person methods

--- app.rb
+++ app.rb
@@ -17,5 +17,68 @@ class App
         @main_menu.comeback
     end
 
+    def list_all_persons
+        $stdout.clear_screen
+        puts 'No persons are available' if @books.empty?
 
+        @persons.each do |id, name, age, classroom, parent_permission| puts 
+            "Id: #{id}\n Name: #{name}\n Age: #{age}\n Type: #{classroom}\n Parent_permission: #{parent_permission}\n"
+        end
+        @main_menu.comeback
+    end
+
+    def create_student
+        print 'Student name: '
+        name = gets.chomp
+        print 'Student age: '
+        age = gets.chomp
+        print 'Student has parent permission [Y/N]: '
+        parent_permission = gets.chomp
+        print 'Student classroom: '
+        classroom = gets.chomp
+        @persons.push(Student.new(name, age.to_i, parent_permission, classroom))
+    end
+
+    def create_teacher 
+        print 'Teacher name: '
+        name = gets.chomp
+        print 'Teacher age: '
+        age = gets.chomp
+        print 'Teacher specialization: '
+        specialization = gets.chomp
+        @persons.push(Teacher.new(name, age.to_i, specialization))
+    end
+
+    def create_person 
+        print "What do you want to create, press [1] Student or [2] Teacher"
+        user_input = $stdin.getch
+        create_student if user_input == '1'
+        create_teacher if user_input == '2'
+
+        puts "** Person created successfully **"
+        @main_menu.comeback
+    end
+
+    def create_book
+        $stdout.clear_screen
+        print "Book title: "
+        title = gets.chomp
+        print "Book author: "
+        author = gets.chomp
+        @books.push(Book.new(title, author))
+        puts "** Book created successfully **"
+        @main_menu.comeback
+    end
+
+    def show_person
+        if @persons.empty?
+            puts '** No person to rent books, please create a new person **'
+            return false
+        end
+        puts 'Select person who\'s renting the book'
+        @persons.each_with_index do |person, index|
+            puts "[#{index}] - #{person.classroom} - Name: #{person.correct_name}"
+        end
+        $stdin.getch.to_i
+    end
 end
